cc31b60025f49889391f10440e88b63fcbdc03f9
kavindu09611 <kavinduwickramasinghe1920@gmail.com>
2024-02-08 23:48:33 +0530

    Render props pattern - 2nd Usecase

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import "./styles.css";
 import Input from "./Input";
 
 export default function App() {
+  const showValue = (value) => <>The value is {value}</>;
+  const multiplyByTen = (value) => <>The multiplied value is {value * 10}</>;
   return (
     <div className="App">
-      <Input renderTextBelow={(value) => <>The value is {value}</>} />
+      <Input renderTextBelow={showValue} />
+      <br />
+      <Input renderTextBelow={multiplyByTen} />
     </div>
   );
 }
